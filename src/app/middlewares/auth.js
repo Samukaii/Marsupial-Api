7f20fa805a1f6ee245bfe5cf860fca561bc0eab1
Samuel <mukinha.2016@gmail.com>
2020-06-05 23:32:13 -0300

    Aplicação modificada para usar .env

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,27 +1,27 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const secret = require('../../config/auth.json').secret;
+const secret = process.env.APP_SECRET;
 
 const WithAuth = (req, res, next) => {
-    const token = req.headers['x-access-token'];
-    if (!token) {
-        res.status(400).send({Error: 'Token not provided'});
-    } else {
-        jwt.verify(token, secret, (err, decoded) => {
-            if (err) res.status(401).send({Error: 'Token invalid'});
-            else {
-                req.email = decoded.email;
-                User.findOne({email: decoded.email})
-                    .then(user => {
-                        req.user = user;
-                        next();
-                    })
-                    .catch(err => {
-                        res.json(err);
-                    });
-            }
-        });
-    }
+  const token = req.headers['x-access-token'];
+  if (!token) {
+    res.status(400).send({Error: 'Token not provided'});
+  } else {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) res.status(401).send({Error: 'Token invalid'});
+      else {
+        req.email = decoded.email;
+        User.findOne({email: decoded.email})
+          .then(user => {
+            req.user = user;
+            next();
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      }
+    });
+  }
 };
 
 module.exports = WithAuth;
--- src/database/index.js
+++ src/database/index.js
@@ -1,15 +1,16 @@
-const databaseUrl = require('./databaseTunnelUrl.json').url;
+//const databaseUrl = require('./databaseTunnelUrl.json').url;
 const mongoose = require('mongoose');
+require('dotenv/config');
 
-var stringConnection = `mongodb://${databaseUrl}/marsupialApi`;
+//var stringConnection = `mongodb://${databaseUrl}/marsupialApi`;
 
 connectDB();
 
 async function connectDB() {
   try {
-    await mongoose.connect(stringConnection, {
+    await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
     console.log('Banco de dados conectado');
   } catch (error) {
