e41b424bd8e85baca1fca8f7d28b1fa43fb7cdd1
Samuel <mukinha.2016@gmail.com>
2020-06-05 22:25:10 -0300

    Adicionado o CORS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 require('./src/app/models/user');
 
 const server = express();
 
 //CONFIGS
 server.use(express.json());
+server.use(cors());
 
 //ROTAS
 require('./src/routes/authentication')(server);
