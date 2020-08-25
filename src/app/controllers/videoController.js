22612d238e208b7cc2933e32a36117e9e703df1e
Samukaii <samuelalejandro.dev@gmail.com>
2020-08-25 21:40:03 +0000

    Atributo link do model video modificado

--- src/app/controllers/videoController.js
+++ src/app/controllers/videoController.js
@@ -2,77 +2,76 @@ const Video = require("../models/video");
 const { to } = require("../../helpers/assyncronous");
 const mongoose = require("mongoose");
 const {
-    loginErrors,
-    registerErrors,
-    unknownErrors,
-    notProvideds
+  loginErrors,
+  registerErrors,
+  unknownErrors,
+  notProvideds,
 } = require("../../config/errors.js");
 const { query } = require("express");
 
 const population = {
-    path: "lesson",
+  path: "lesson",
+  populate: {
+    path: "section",
     populate: {
-        path: "section",
-        populate: {
-            path: "subject",
-            populate: {
-                path: "knowledge"
-            }
-        }
-    }
+      path: "subject",
+      populate: {
+        path: "knowledge",
+      },
+    },
+  },
 };
 
 function isAnObjectId(string) {
-    if (!string) return false;
-    try {
-        mongoose.Types.ObjectId(string)
-        return true;
-    } catch (error) {
-        return false;
-    }
+  if (!string) return false;
+  try {
+    mongoose.Types.ObjectId(string);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 module.exports = {
-    async index(req, res) {
-        const { page = 1, limit = 20, lesson } = req.query;
-        const query = isAnObjectId(lesson) ? { lesson } : {};
-        const video = await Video.paginate(query, { page: page, limit: limit });
-        return res.json(video);
-    },
-    async show(req, res) {
-        const video = await Video.findById(req.params.id);
+  async index(req, res) {
+    const { page = 1, limit = 20, lesson } = req.query;
+    const query = isAnObjectId(lesson) ? { lesson } : {};
+    const video = await Video.paginate(query, { page: page, limit: limit });
+    return res.json(video);
+  },
+  async show(req, res) {
+    const video = await Video.findById(req.params.id);
 
-        video.populate(population, (err, resp) => {
-            res.json(resp);
-        });
-    },
-    async store(req, res) {
-        const { title, link, lesson } = req.body;
+    video.populate(population, (err, resp) => {
+      res.json(resp);
+    });
+  },
+  async store(req, res) {
+    const { title, linkVideo, lesson } = req.body;
 
-        if (!title) return res.status(400).send(notProvideds.titleNotProvided);
+    if (!title) return res.status(400).send(notProvideds.titleNotProvided);
 
-        if (!link) return res.status(400).send(notProvideds.linkNotProvided);
+    if (!linkVideo) return res.status(400).send(notProvideds.linkNotProvided);
 
-        if (!lesson)
-            return res.status(400).send(notProvideds.lessonNotProvided);
+    if (!lesson) return res.status(400).send(notProvideds.lessonNotProvided);
 
-        const [video, error] = await to(Video.create(req.body));
-        if (error)
-            res.status(400).send({
-                error: "Erro ao adicionar vídeo " + error,
-                code: unknownErrors.unknownCreateVideoErrorCode
-            });
-        return res.status(201).send(video);
-    },
-    async update(req, res) {
-        const video = await Video.findByIdAndUpdate(req.params.id, req.body, {
-            new: true
-        });
-        return res.json(video);
-    },
-    async destroy(req, res) {
-        await Video.findByIdAndRemove(req.params.id);
+    const [video, error] = await to(Video.create(req.body));
+    if (error)
+      res.status(400).send({
+        error: "Erro ao adicionar vídeo " + error,
+        code: unknownErrors.unknownCreateVideoErrorCode,
+      });
+    return res.status(201).send(video);
+  },
+  async update(req, res) {
+    const video = await Video.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    return res.json(video);
+  },
+  async destroy(req, res) {
+    await Video.findByIdAndRemove(req.params.id);
 
-        return res.status(204).send();
-    }
+    return res.status(204).send();
+  },
 };
--- src/app/models/video.js
+++ src/app/models/video.js
@@ -1,30 +1,30 @@
-const mongoose = require('../../database');
-const mongoosePaginate = require('mongoose-paginate');
+const mongoose = require("../../database");
+const mongoosePaginate = require("mongoose-paginate");
 
 const VideoSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
+  title: {
+    type: String,
+    required: true,
+  },
+  linkVideo: {
+    type: String,
+    required: true,
+  },
+  lesson: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Lesson",
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  qualitys: [
+    {
+      type: String,
     },
-    link: {
-        type: String,
-        required: true
-    },
-    lesson: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Lesson',
-        required: true
-    },
-    description: {
-        type: String
-    },
-    qualitys: [
-        {
-            type: String
-        }
-    ]
+  ],
 });
 
 VideoSchema.plugin(mongoosePaginate);
 
-module.exports = mongoose.model('Video', VideoSchema);
+module.exports = mongoose.model("Video", VideoSchema);
